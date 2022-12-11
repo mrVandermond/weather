f7aa0fd20f6434060076d40ca80ceb26ec97564b
mrVandermond <kirill.guyda32@gmail.com>
2022-12-11 13:00:09 +0400

    create promisify function to get geolocation and create component to handle geolocation errors

---
+++ src/components/ErrorGeolocation.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Ошибка при получении геоданных. Попробуйте изменить настройки браузера</div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
