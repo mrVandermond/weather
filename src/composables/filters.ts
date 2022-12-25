8bb23f13a139c03a9cc9818e9733343b654fcb10
mrVandermond <kirill.guyda32@gmail.com>
2022-12-25 12:38:53 +0400

    added new filter functions

--- src/composables/filters.ts
+++ src/composables/filters.ts
@@ -4,12 +4,38 @@ function capitalize(str: string): string {
   return `${str[0].toUpperCase()}${str.slice(1)}`;
 }
 
+function hoursWithLeadingZero(timestamp: number): string {
+  const hours = new Date(timestamp).getHours();
+
+  if (hours < 10) return `0${hours}:00`;
+
+  return `${hours}:00`;
+}
+
+function dayOfWeek(timestamp: number): string {
+  const dayOfWeekArr = [
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+  ];
+
+  return dayOfWeekArr[new Date(timestamp).getDay()];
+}
+
 interface IFilters {
   capitalize: typeof capitalize;
+  hoursWithLeadingZero: typeof hoursWithLeadingZero;
+  dayOfWeek: typeof dayOfWeek;
 }
 
 export function useFilters(): IFilters {
   return {
     capitalize,
+    hoursWithLeadingZero,
+    dayOfWeek,
   };
 }
